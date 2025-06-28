bb7c71f63bc6100ab9aeb118ef9ceeabe5c03e74
Veyronity <veyronitymc@gmail.com>
2025-06-28 16:04:05 +0300

    Init

---
+++ src/main/java/org/breakthebot/townyPacts/TownyPacts.java
@@ -0,0 +1,15 @@
+package org.breakthebot.townyPacts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownyPacts extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
