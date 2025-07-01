1739eb0d85b982655101c0ec5de952f9d1c16a98
Jinzo <91829829+pernio@users.noreply.github.com>
2025-07-01 11:39:14 +0200

    Commands registered + logic renaming

---
+++ src/main/java/org/breakthebot/townyPacts/commands/infoPact.java
@@ -0,0 +1,11 @@
+package org.breakthebot.townyPacts.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class infoPact {
+    public static boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull[] args) {
+        return false;
+    }
+}
